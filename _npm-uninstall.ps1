03e78f13f684a645f6bc0ef9c9f92cd1a63ac346
yaoyinyu <3181484805@qq.com>
2026-08-15 19:30:16 +0800

    Delete _npm-uninstall.ps1

--- _npm-uninstall.ps1
+++
@@ -1,3 +0,0 @@
-Set-Location "E:\AI Project\ClaudeCode\JiaRu"
-npm uninstall "@mediapipe/camera_utils" "@mediapipe/drawing_utils" ngrok 2>&1 | Out-File "$env:TEMP\npm-uninstall.txt" -Encoding utf8
-Write-Host "Exit: $LASTEXITCODE"
