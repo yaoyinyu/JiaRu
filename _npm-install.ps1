56847dfd88f39054bffc9e5e3e386d4982e17f8c
yaoyinyu <3181484805@qq.com>
2026-08-15 19:30:08 +0800

    Delete _npm-install.ps1

--- _npm-install.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Continue"
-Set-Location "E:\AI Project\ClaudeCode\JiaRu"
-& npm install *>&1 | ForEach-Object { $_.ToString() } | Out-File "$env:TEMP\npm-install.txt" -Encoding utf8
-Write-Host "Exit: $LASTEXITCODE"
